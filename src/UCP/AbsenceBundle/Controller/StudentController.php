691afd72a6130d2116f1c53955e6b5c80b18a962
Mathieu Darse <mattdarse@gmail.com>
2012-10-14 23:11:26 +0200

    Add student list action

--- src/UCP/AbsenceBundle/Controller/StudentController.php
+++ src/UCP/AbsenceBundle/Controller/StudentController.php
@@ -5,15 +5,21 @@ namespace UCP\AbsenceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 class StudentController extends Controller
 {
     /**
+     * @Secure(roles="ROLE_SECRETARY, ROLE_ADMIN")
      * @Route("/student")
      * @Template()
      */
     public function indexAction()
     {
-        return array();
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('UCPAbsenceBundle:Student');
+        $students = $repo->findAll();
+
+        return array('students' => $students);
     }
 }
