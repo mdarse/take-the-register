85ad8d37f9ef5d7430e110001b303aea55d73f7d
Mathieu Darse <mattdarse@gmail.com>
2012-10-16 00:24:39 +0200

    Make admin

--- src/UCP/AbsenceBundle/Admin/UserAdmin.php
+++ src/UCP/AbsenceBundle/Admin/UserAdmin.php
@@ -7,42 +7,61 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 
 class UserAdmin extends Admin
 {
     protected $baseRouteName = 'user_admin';
-    protected $baseRoutePattern = '/';
+    protected $baseRoutePattern = 'user';
+
+    protected function configureShowField(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('username')
+            ->add('email')
+            ->add('phone')
+            ->add('enabled')
+            ->add('lastLogin')
+            ->add('roles')
+        ;
+    }
 
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('name')
-            ->add('enabled', null, array('required' => false))
+            ->add('username')
+            ->add('email')
+            ->add('phone')
+            ->add('enabled')
         ;
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
-            ->add('name')
-            ->add('posts')
+            ->add('username')
+            ->add('email')
+            ->add('phone')
+            ->add('enabled')
+            ->add('lastLogin')
+            ->add('roles')
         ;
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->addIdentifier('name')
-            ->add('slug')
-            ->add('enabled')
+            ->addIdentifier('username')
+            ->add('email')
+            ->add('lastLogin')
         ;
     }
 
     public function validate(ErrorElement $errorElement, $object)
     {
         $errorElement
-            ->with('name')
-                ->assertMaxLength(array('limit' => 32))
+            ->with('username')
+                ->assertNotBlank()
             ->end()
         ;
     }
