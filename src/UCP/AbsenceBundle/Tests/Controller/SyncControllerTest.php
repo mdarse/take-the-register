56816e31e573e77e74f69b57f6694db57b6148f9
Mathieu Darse <mattdarse@gmail.com>
2012-11-19 20:43:05 +0100

    Add dummy test to make Travis CI happy :-)

---
+++ src/UCP/AbsenceBundle/Tests/Controller/SyncControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace UCP\AbsenceBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SyncControllerTest extends WebTestCase
+{
+    public function testFakeScenario()
+    {
+        $this->assertTrue(true);
+    }
+}
