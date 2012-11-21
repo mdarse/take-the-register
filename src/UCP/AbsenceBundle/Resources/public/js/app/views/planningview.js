050b345486d25affca656318f34c1cfe1450074c
Mathieu Darse <mattdarse@gmail.com>
2012-11-21 03:00:58 +0100

    Bind events on PlanningView

--- src/UCP/AbsenceBundle/Resources/public/js/app/views/planningview.js
+++ src/UCP/AbsenceBundle/Resources/public/js/app/views/planningview.js
@@ -2,6 +2,7 @@ App.Views.PlanningView = Backbone.View.extend({
 
     initialize: function() {
         this.template = Handlebars.templates.planning;
+        this.collection.on('add change remove reset', this.render, this);
     },
 
     render: function() {
