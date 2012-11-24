8e7399b3c9aadd9520f0d9249aa45018a6b4bc32
Romain Bellina <contact@romainbellina.fr>
2012-11-24 21:24:15 +0100

    Update planningView commit test

--- src/UCP/AbsenceBundle/Resources/public/js/app/views/planningview.js
+++ src/UCP/AbsenceBundle/Resources/public/js/app/views/planningview.js
@@ -8,14 +8,39 @@ App.Views.PlanningView = Backbone.View.extend({
     },
 
     render: function() {
+        //transformation de l'objet lessons en tableau
         var lessons = this.lessons.toArray();
+        var lessonsDay = new Array();
+        var lessonsDate;
+        //Récuperation de la date pour trier les lessons au format YYY-MM-DD
+        var currentLessonDate = lessons[0].get('start');
+        currentLessonDate = currentLessonDate.split(" ");
+        currentLessonDate = currentLessonDate[0];
+        //parcour du tableau 'lessons'
         for (var i = 0, l = lessons.length; i < l; i++) {
-            var view = new App.Views.PlanningDayView({
-                date: lessons[i].get('start'),
-                lessons: [lessons[i]]
-            });
-            view.render();
-            this.$el.append(view.el);
+           //Recuperation de la date de la lesson en cours
+           lessonsDate = lessons[i].get('start');
+           lessonsDate = lessonsDate.split(" ");
+
+           if(lessonsDate[0] == currentLessonDate){
+               lessonsDay.push(lessons[i]);
+           }
+           else{
+                //création de la vue
+                var view = new App.Views.PlanningDayView({
+                    date: currentLessonDate,
+                    lessons: lessonsDay
+                });
+                view.render();
+                this.$el.append(view.el);
+                //réinitialisation du tableau
+                lessonsDay=[];
+                //Date suivante
+                currentLessonDate = lessons[i].get('start');
+                currentLessonDate = currentLessonDate.split(" ");
+                currentLessonDate = currentLessonDate[0];
+                lessonsDay.push(lessons[i]);
+           }
         };
         return this;
     }
