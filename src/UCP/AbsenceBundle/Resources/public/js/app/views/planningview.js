c028c803dbdd7a9e0e47533480c5cf83d2311a60
Mathieu Darse <mattdarse@gmail.com>
2012-11-24 13:19:33 +0100

    Implement planning grouping by day

--- src/UCP/AbsenceBundle/Resources/public/js/app/views/planningview.js
+++ src/UCP/AbsenceBundle/Resources/public/js/app/views/planningview.js
@@ -9,15 +9,37 @@ App.Views.PlanningView = Backbone.View.extend({
 
     render: function() {
         var lessons = this.lessons.toArray();
-        for (var i = 0, l = lessons.length; i < l; i++) {
+        // Build day views and keep DOM elements for insertion
+        var elements = [];
+        this.groupByDay(lessons, function(date, lessons) {
             var view = new App.Views.PlanningDayView({
-                date: lessons[i].get('start'),
-                lessons: [lessons[i]]
+                date: date,
+                lessons: lessons
             });
             view.render();
-            this.$el.append(view.el);
-        };
+            elements.push(view.el);
+        });
+        this.$el.append(elements);
         return this;
+    },
+
+    groupByDay: function(lessons, iterator) {
+        // Lessons are expected in date sorted order
+        var lastDate, // The day we are working on
+            lessonDate, // The date of of currently evaluated lesson
+            lessonBag = []; // Temporary array holding holding lessons between each iterator call
+
+        _.each(lessons, function(lesson) {
+            lessonDate = new Date(lesson.get('start'));
+            lessonDate.setHours(0,0,0,0); // We strip time components to compare date reliablely
+            if (lessonDate > lastDate) {
+                // We changed of day: call iterator and reset lesson bag
+                iterator(lastDate, lessonBag);
+                lessonBag = [];
+            }
+            lessonBag.push(lesson);
+            lastDate = lessonDate;
+        });
     }
 
 });
