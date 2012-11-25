d2d89dba5b813073821a320ea3c3582274f7440e
Romain Bellina <contact@romainbellina.fr>
2012-11-25 16:44:33 +0100

    Update command check statut lessons

--- src/UCP/AbsenceBundle/Command/verifyStatutCommand.php
+++ src/UCP/AbsenceBundle/Command/verifyStatutCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/*
 class verifyStatutCommand extends ContainerAwareCommand
 {
     protected function configure()
@@ -25,10 +26,14 @@ class verifyStatutCommand extends ContainerAwareCommand
         //Récupere tous les cours qui on lieu en ce moment
         $em = $this->_container->get('doctrine.orm.entity_manager');
         $repo = $em->getRepository('UCPAbsenceBundle:Lesson');
-        $lessons = $repo->findAll()
+        $lessons = $repo->findTodayOrUpcomingLessons(
+                            array('start' => <date>),
+                            array('start' => 'ASC'),
+                            20);
         //Si date actuel > date debut + 30 min alors on envoi un mail de rappel toute les 5 min sauf s'il y a deja des absent de selectionné
         //Si date actuel > date benut + 60min alors on envoi un mail avec la liste des absents.
     }
 }
+*/
 
 ?>
