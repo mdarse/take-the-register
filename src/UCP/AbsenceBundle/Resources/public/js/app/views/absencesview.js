d8eba2a2d7d69b2f8dedd925293a0599861297bb
Mathieu Darse <mattdarse@gmail.com>
2012-11-26 23:44:31 +0100

    WIP AbsencesView

--- src/UCP/AbsenceBundle/Resources/public/js/app/views/absencesview.js
+++ src/UCP/AbsenceBundle/Resources/public/js/app/views/absencesview.js
@@ -1,10 +1,23 @@
 App.Views.AbsencesView = Backbone.View.extend({
 
+    initialize: function() {
+        // var oneWeekAgo = new Date();
+        // oneWeekAgo.setH
+
+        // this.criteria = new Backbone.Model.extend({
+        //     defaults: {
+        //         //from: oneWeekAgo,
+        //         //to: now,
+        //         studentId: null
+        //     }
+        // });
+    },
+
     render: function() {
         this.$el.empty();
-        this.$el.html("Hello World");
+        // this.$el.html("Hello World");
 
-        this.$el.append("truc");
+        // this.$el.append("truc");
 
         return this;
     }
